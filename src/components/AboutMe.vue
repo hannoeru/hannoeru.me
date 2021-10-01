dc5a6c65e43d3c1d37bf0dddd9794ebbf08ab23b
hanlee <me@hanlee.co>
2021-10-01 22:49:18 +0900

    chore: remove lazy load on home page image

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col md:flex-row items-center pt-4 mb-12">
     <div class="relative w-45 h-45 rounded-full overflow-hidden flex-shrink-0 md:mr-10 -md:mb-10">
-      <img v-lazy="'/avatar.png'" alt="Han" class="absolute w-full h-full object-cover !m-0">
+      <img src="/avatar.png" alt="Han" class="absolute w-full h-full object-cover !m-0">
     </div>
     <div>
       <h1 class="!mb-4">
