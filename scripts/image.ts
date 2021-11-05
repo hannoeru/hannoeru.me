9388b76e8d18a543fe620af8bd6ebd07a791f238
hanlee <me@hanlee.co>
2021-11-05 15:44:07 +0900

    feat: cache optimized images

--- scripts/image.ts
+++ scripts/image.ts
@@ -1,3 +1,5 @@
+import { dirname } from 'path'
+import fs from 'fs-extra'
 import fg from 'fast-glob'
 import sharp from 'sharp'
 import chalk, { ForegroundColor } from 'chalk'
@@ -18,22 +20,33 @@ export async function optimizeImages() {
 
   await Promise.all(
     images.map(async(image) => {
-      const outFile = image.replace('public', 'dist')
-      let img = sharp(image)
-      const meta = await img.metadata()
+      const outFile = image.replace('public', 'temp')
+      const generateImg = async() => {
+        let img = sharp(image)
+        const meta = await img.metadata()
 
-      if (meta.width && meta.width > 1280)
-        img = img.resize({ width: 1280 })
+        if (meta.width && meta.width > 1280)
+          img = img.resize({ width: 1280 })
 
-      if (meta.format === 'png')
-        img = img.png({ palette: true })
+        if (meta.format === 'png')
+          img = img.png({ palette: true })
 
-      const info = await img.toFile(outFile)
+        await fs.ensureDir(dirname(outFile))
 
-      // eslint-disable-next-line no-console
-      console.log(
-        `${chalk.dim('dist/')}${chalk.cyan(image.replace('public/', '').padEnd(15, ' '))}  ${chalk.dim(getSize(info.size))}`,
-      )
+        const info = await img.toFile(outFile)
+
+        // eslint-disable-next-line no-console
+        console.log(
+          `${chalk.dim('dist/')}${chalk.cyan(image.replace('public/', '').padEnd(15, ' '))}  ${chalk.dim(getSize(info.size))}`,
+        )
+
+        return info
+      }
+
+      if (fs.existsSync(outFile))
+        await fs.copy(outFile, outFile.replace('temp', 'dist'))
+      else
+        await generateImg()
     }),
   )
   log('Images optimized', 'green')
