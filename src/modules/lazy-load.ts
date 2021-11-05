f84d2ea9c4b9bff8e2245d01c60c281da2d92092
hanlee <me@hanlee.co>
2021-11-05 15:50:58 +0900

    feat: apply image size to element when loading image

--- src/modules/lazy-load.ts
+++ src/modules/lazy-load.ts
@@ -1,8 +1,15 @@
-import { Lazyload } from '@vant/lazyload'
+import { Lazyload, LazyloadOptions } from '@vant/lazyload'
 import { UserModule } from '@/types'
 
 export const install: UserModule = ({ app }) => {
   app.use(Lazyload, {
     observer: true,
-  })
+    adapter: {
+      // set default image weight & height
+      loading({ el, naturalHeight, naturalWidth }) {
+        el.setAttribute('width', naturalWidth)
+        el.setAttribute('height', naturalHeight)
+      },
+    },
+  } as LazyloadOptions)
 }
