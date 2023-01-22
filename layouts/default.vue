14e591f08f6ed27b62fe4a52a378d6dec3c30e5d
hanlee <me@hanlee.co>
2023-01-22 16:14:04 +0900

    fix: mobile size markdown overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-screen-xl mx-auto grid md:gap-6 md:grid-cols-[220px_1fr] p-0 md:p-12">
     <Header />
-    <main class="pt-20 md:pt-$header-height bg-white dark:bg-dark-900 p-10 md:rounded-2xl">
+    <main class="pt-20 md:pt-$header-height bg-white dark:bg-dark-900 p-10 md:rounded-2xl w-screen">
       <slot />
       <Footer />
     </main>
