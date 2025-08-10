a09c75fb80f32b3f33e4be0c562417b0e516c509
hanlee <me@hanlee.co>
2025-08-10 14:27:58 +0900

    feat: dedupe deps

--- server/routes/feed.json.ts
+++ server/routes/feed.json.ts
@@ -1,5 +1,3 @@
-import { createFeed } from '../utils/feed'
-
 export default defineEventHandler(async (event) => {
   const feed = await createFeed(event)
   setResponseHeader(event, 'content-type', 'application/json')
--- server/routes/feed.xml.ts
+++ server/routes/feed.xml.ts
@@ -1,5 +1,3 @@
-import { createFeed } from '~~/server/utils/feed'
-
 export default defineEventHandler(async (event) => {
   const feed = await createFeed(event)
   setResponseHeader(event, 'content-type', 'text/xml')
