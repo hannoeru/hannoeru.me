0e92b02abf375df1bfb2304e3b094c36ab89e431
hanlee <me@hanlee.co>
2025-09-10 14:42:23 +0900

    chore: update deps

--- server/utils/feed.ts
+++ server/utils/feed.ts
@@ -1,4 +1,4 @@
-import { queryCollection } from '@nuxt/content/server'
+import { queryCollection } from '@nuxt/content/nitro'
 import dayjs from 'dayjs'
 import type { FeedOptions } from 'feed'
 import { Feed } from 'feed'
